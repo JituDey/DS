1cd8a77490298497ff1ff25df3b6fd005ababff9
Jitu Rani Dey <JituRani.Dey@jda.com>
2017-10-05 17:23:09 +0530

    Random pointer tree clonning modified with map

--- tree/TreeCloning.java
+++ tree/TreeCloning.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class TreeCloning{
 	public static void main(String[] args){
 		Node node = new Node(10);
@@ -14,6 +16,9 @@ class TreeCloning{
 		Node newRoot = TreeCloning.cloneTree(node);
 		System.out.println("---cloned tree--");
 		Tree.levelOrderTraversal(newRoot);
+		System.out.println("---cloned tree iteration--");
+		Node newRoot2 = TreeCloning.cloneTreeIterative(node);
+		Tree.levelOrderTraversal(newRoot2);
 		
 		ConnectedNode connectedNode = new ConnectedNode(10);
 		connectedNode.nextElement=null;
@@ -29,7 +34,9 @@ class TreeCloning{
 		connectedNode.right.right=new ConnectedNode(17);
 		connectedNode.left.right.nextElement=connectedNode.right.left;
 		connectedNode.right.left.nextElement=connectedNode.right.right;
-		ConnectedNode newConnectedRoot = TreeCloning.cloneSpecialTree(connectedNode);
+		ConnectedNode newConnectedRoot = cloneSpecialTree(connectedNode);
+		System.out.println("---actual special tree--");
+		ConnectedTree.printSiblings(connectedNode.left.left, connectedNode, 1);
 		System.out.println("---cloned special tree--");
 		ConnectedTree.printSiblings(newConnectedRoot.left.left, newConnectedRoot, 1);
 	}
@@ -44,15 +51,55 @@ class TreeCloning{
 		return newRoot;
 	}
 	
-	public static ConnectedNode cloneSpecialTree(ConnectedNode root){
-		if(root==null) return null;
+	private static ConnectedNode cloneSpecialTree(ConnectedNode root) {
+        Map<ConnectedNode,ConnectedNode> map = new HashMap<>();
+        copyLeftRight(root, map);
+        storeLinks(root,map);
+        return map.get(root);
+    }
+	
+	public static void copyLeftRight(ConnectedNode root, Map<ConnectedNode, ConnectedNode> map){
+		if(root==null) return;
 		ConnectedNode newRoot = new ConnectedNode(root.data);
+		map.put(root, newRoot);
 		if(root.left!=null)
-			newRoot.left = cloneSpecialTree(root.left);
+			copyLeftRight(root.left, map);
 		if(root.right!=null)
-			newRoot.right = cloneSpecialTree(root.right);
-		if(root.nextElement!=null)
-			newRoot.nextElement = cloneSpecialTree(root.nextElement);
-		return newRoot;
+			copyLeftRight(root.right, map);
+	}
+	public static void storeLinks(ConnectedNode root, Map<ConnectedNode, ConnectedNode> map){
+		if(root==null) return;
+		ConnectedNode node = map.get(root);
+		if(node!=null){
+			node.left = map.get(root.left);
+			node.right = map.get(root.right);
+			node.nextElement = map.get(root.nextElement);
+			storeLinks(root.left, map);
+			storeLinks(root.right, map);
+		}
+	}
+	public static Node cloneTreeIterative(Node root){
+		if(root==null) return null;
+		Queue<Node> q1 = new LinkedList<Node>();
+		Queue<Node> q2 = new LinkedList<Node>();
+		q1.add(root);
+		Node root2= new Node(root.data);
+		q2.add(root2);
+		
+		while(!q1.isEmpty()){
+			Node n1 = q1.remove();
+			Node n2 = q2.remove();
+			if(n1.left!=null){
+				q1.add(n1.left);
+				n2.left=new Node(n1.left.data);
+				q2.add(n2.left);
+			}
+			if(n1.right!=null){
+				q1.add(n1.right);
+				n2.right=new Node(n1.right.data);
+				q2.add(n2.right);
+			}
+		}
+		return root2;
 	}
 }
