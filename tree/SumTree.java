a40c1699f578f4a380d7423f0d64dcb24b9f47ac
Jitu Rani Dey <JituRani.Dey@jda.com>
2017-10-11 08:49:31 +0530

    cousin nodes check added

--- tree/SumTree.java
+++ tree/SumTree.java
@@ -38,6 +38,14 @@ public class SumTree {
         sumTreeNode.left.right = new Node(6);
         sumTreeNode.right.right = new Node(3);
 		System.out.println("Given tree is a SumTree: "+ ifSumTree(sumTreeNode));
+		Node root1 = new Node(10);
+        root1.left = new Node(8);
+        root1.right = new Node(2);
+        root1.left.left = new Node(3);
+        root1.left.right = new Node(5);
+		root1.right.right = new Node(9);
+		System.out.println("Given tree's children satisfies sum property : "+ ifChildrenSumPropertySafisfied(root1));
+		System.out.println("Given nodes are cousin nodes : "+ ifCousin(root1, root1.left.left, root1.right.right));
 	}
 	
 	public static int convertToSumTree(Node root) {
@@ -76,5 +84,68 @@ public class SumTree {
 			return root.data;
 		}
 		return root.data+sumofSubTrees(root.left)+sumofSubTrees(root.right);
+	}
+	
+	/*Check for Children Sum Property in a Binary Tree
+					  10
+					/   \
+				  8       2
+				/    \   / 
+			   3      5  2   
+	*/
+	public static boolean ifChildrenSumPropertySafisfied(Node root) {
+		if(root==null) return true;		
+		int sum = 0;
+		if(root.left==null && root.right==null) return true;
+		if(root.left!=null) sum+=root.left.data;
+		if(root.right!=null) sum+=root.right.data;
+		if(root.data==sum) {
+			return ifChildrenSumPropertySafisfied(root.left) && ifChildrenSumPropertySafisfied(root.right);
+		}
+		System.out.println("Property not satisfied for :"+ root.data +" and sum is:"+ sum);
+		return false;
+	}
+	
+	public static int getSumOfChildren(Node root) {
+		if(root==null) return 0;
+		return getSumOfChildren(root.left) + getSumOfChildren(root.right);
+	}
+	
+	/*
+	 Cousin nodes are the nodes who are at the same level in the tree and whose parents are siblings.
+		 6
+	   /   \
+	  3     5
+	 / \   / \
+	7   8 1   3
+	Say two node be 7 and 1, result is TRUE.
+	Say two nodes are 3 and 5, result is FALSE.
+	Say two nodes are 7 and 5, result is FALSE.
+	Check the height of both the nodes, if heights are dif­fer­ent then return false.
+	Check if both the nodes has the same parent, if yes then return false.
+	else return true.
+	*/
+	public static boolean ifCousin(Node root, Node n1, Node n2) {
+		if(root==null) return true;		
+		int hN1=getHeightOfNode(root, n1,1);
+		int hN2=getHeightOfNode(root, n2,1);
+		if(hN1 != hN2) return false;
+		if(isSameParents(root, n1, n2)) return false;
+		return true;
+	}
+	
+	public static int getHeightOfNode(Node root, Node n, int height){
+		if(root==null) return 0;
+		if(root.data == n.data) return height;
+		int level = getHeightOfNode(root.left, n, height+1);
+		if(level==0) 
+			level= getHeightOfNode(root.right, n, height+1);
+		return level;
+	}
+	
+	public static boolean isSameParents(Node root, Node n1, Node n2) {
+		if(root==null) return false;
+		return ((root.left==n1 && root.right==n2) || (root.right==n1 && root.left==n2) ||
+				isSameParents(root.left, n1,n2) || isSameParents(root.right, n1,n2));
 	}
 }
